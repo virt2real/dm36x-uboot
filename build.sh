5e865419b6bf335c2efde14e31376a137bfb9fc6
Frank Hunleth <fhunleth@troodon-software.com>
2012-09-16 11:48:53 -0700

    build.sh: Add reference to buildroot for compiler

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-make CROSS_COMPILE=arm-none-linux-gnueabi- clean
-make CROSS_COMPILE=arm-none-linux-gnueabi- davinci_dm365evm_config
-make CROSS_COMPILE=arm-none-linux-gnueabi- 
+#!/bin/sh
+
+# Set BUILDROOT_BASE to the base of the buildroot directory. Make sure
+# that the toolchain has been built.
+
+CROSS_COMPILE=$BUILDROOT_BASE/output/host/usr/bin/arm-none-linux-gnueabi-
+
+make CROSS_COMPILE=$CROSS_COMPILE clean
+make CROSS_COMPILE=$CROSS_COMPILE davinci_dm365evm_config
+make CROSS_COMPILE=$CROSS_COMPILE
 
